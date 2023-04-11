f886464b6c6437ae223b22fcaff0a16d726d2e4d
JASON YOUNG <moorslf@gmail.com>
2023-04-11 00:43:29 -0400

    Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record){
+    let win = record.find(function(object){
+        return object.result === "W";
+    });
+    
+    if (win){
+        return win.year;
+    } else {
+        return undefined;
+    }
+ }
